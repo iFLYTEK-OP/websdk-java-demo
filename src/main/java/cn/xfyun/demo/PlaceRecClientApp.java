2c4cafcc1b1e61b5b120f6de705d37a57e948fef
mqgao <605927229@qq.com>
2021-07-27 11:13:30 +0800

    Create PlaceRecClientApp.java
    1. 新增场所识别

---
+++ src/main/java/cn/xfyun/demo/PlaceRecClientApp.java
@@ -0,0 +1,38 @@
+import cn.xfyun.api.PlaceRecClient;
+import cn.xfyun.config.PropertiesConfig;
+import sun.misc.IOUtils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.util.Base64;
+
+public class PlaceRecClientApp {
+
+    private static final String APP_ID = PropertiesConfig.getAppId();
+    private static final String LTP_KEY = PropertiesConfig.getLtpKey();
+    private static final String apiSecret = PropertiesConfig.getApiSecret();
+
+    private static String filePath = "xxxxxxxx";
+    private static String resourcePath;
+
+    static {
+        try {
+            resourcePath = SilentDetectionClientApp.class.getResource("/").toURI().getPath();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        // 场所识别
+        PlaceRecClient client = new PlaceRecClient
+                .Builder(appId, apiKey, apiSecret)
+                .build();
+        InputStream inputStream = new FileInputStream(new File(resourcePath + filePath));
+        byte[] imageByteArray = IOUtils.readFully(inputStream, -1, true);
+        String imageBase64 = Base64.getEncoder().encodeToString(imageByteArray);
+        System.out.println(client.send( imageBase64, "jpg"));
+    }
+}
