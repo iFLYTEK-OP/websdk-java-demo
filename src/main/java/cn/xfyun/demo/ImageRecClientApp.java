fa4d7d6a5ba6ff60a95525085f1a6fba34209e42
mqgao <605927229@qq.com>
2021-07-27 11:19:34 +0800

    Create ImageRecClientApp.java

---
+++ src/main/java/cn/xfyun/demo/ImageRecClientApp.java
@@ -0,0 +1,45 @@
+import cn.xfyun.api.ImageRecClient;
+import cn.xfyun.config.ImageRecEnum;
+import cn.xfyun.config.PropertiesConfig;
+import sun.misc.IOUtils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.util.Base64;
+
+
+public class LtpClientApp {
+
+    private static final String APP_ID = PropertiesConfig.getAppId();
+    private static final String apiKey = PropertiesConfig.getApiKey();
+  
+    private static String filePath = "xxxxxxxx";
+    private static String resourcePath;
+
+    static {
+        try {
+            resourcePath = SilentDetectionClientApp.class.getResource("/").toURI().getPath();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            ImageRecClient client = new ImageRecClient
+                 //  ImageRecEnum.SCENE     场景识别
+                 //  ImageRecEnum.CURRENCY  物体识别
+                .Builder(appId, apiKey, ImageRecEnum.SCENE)
+                .build();
+            InputStream inputStream = new FileInputStream(new File(resourcePath + filePath));
+            byte[] imageByteArray = IOUtils.readFully(inputStream, -1, true);
+            String imageBase64 = Base64.getEncoder().encodeToString(imageByteArray);
+            System.out.println(client.send( "测试", imageByteArray));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("错误码查询链接：https://www.xfyun.cn/document/error-code");
+        }
+    }
+}
