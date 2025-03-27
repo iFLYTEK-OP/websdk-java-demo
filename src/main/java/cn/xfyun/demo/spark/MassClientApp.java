263a72020af0c52eb1a377cc2d08a88b502a3f53
zyding6 <zyding6@iflytek.com>
2025-03-27 11:07:07 +0800

    added: 大模型语音转写、精调大模型炖、超拟人合成sdk和demo提交

---
+++ src/main/java/cn/xfyun/demo/spark/MassClientApp.java
@@ -0,0 +1,127 @@
+package cn.xfyun.demo.spark;
+
+import cn.hutool.json.JSONUtil;
+import cn.xfyun.api.MassClient;
+import cn.xfyun.config.PropertiesConfig;
+import cn.xfyun.model.RoleContent;
+import cn.xfyun.model.finetuning.response.MassResponse;
+import cn.xfyun.service.finetuning.AbstractMassWebSocketListener;
+import cn.xfyun.util.StringUtils;
+import okhttp3.Response;
+import okhttp3.WebSocket;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * （fine-tuning-text）精练大模型文本对话
+ * 1、APPID、APISecret、APIKey信息获取：<a href="https://training.xfyun.cn/model/add">...</a>
+ * 2、文档地址：<a href="https://www.xfyun.cn/doc/spark/%E7%B2%BE%E8%B0%83%E6%9C%8D%E5%8A%A1API-websocket.html">...</a>
+ */
+public class MassClientApp {
+    private static final Logger logger = LoggerFactory.getLogger(MassClientApp.class);
+    private static final String appId = PropertiesConfig.getAppId();
+    private static final String apiKey = PropertiesConfig.getApiKey();
+    private static final String apiSecret = PropertiesConfig.getApiSecret();
+
+
+    public static void main(String[] args) throws Exception {
+        MassClient client = new MassClient.Builder()
+                // .signatureWs("0", "xdeepseekv3", appId, apiKey, apiSecret)
+                .signatureWs("0", "xdeepseekr1", appId, apiKey, apiSecret)
+                // .signatureHttp("0", "xdeepseekr1", apiKey)
+                .wsUrl("wss://maas-api.cn-huabei-1.xf-yun.com/v1.1/chat")
+                // .requestUrl("https://maas-api.cn-huabei-1.xf-yun.com/v1")
+                .build();
+
+        List<RoleContent> messages = new ArrayList<>();
+        RoleContent roleContent = new RoleContent();
+        roleContent.setRole("user");
+        roleContent.setContent("你好");
+        RoleContent roleContent1 = new RoleContent();
+        roleContent1.setRole("assistant");
+        roleContent1.setContent("你好！");
+        RoleContent roleContent2 = new RoleContent();
+        roleContent2.setRole("user");
+        roleContent2.setContent("你是谁");
+        RoleContent roleContent3 = new RoleContent();
+        roleContent3.setRole("assistant");
+        roleContent3.setContent("我是Spark API。");
+        RoleContent roleContent4 = new RoleContent();
+        roleContent4.setRole("user");
+        roleContent4.setContent("帮我讲一个笑话");
+
+        messages.add(roleContent);
+        messages.add(roleContent1);
+        messages.add(roleContent2);
+        messages.add(roleContent3);
+        messages.add(roleContent4);
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyy-MM-dd HH:mm:ss.SSS");
+        Date dateBegin = new Date();
+
+        StringBuffer finalResult = new StringBuffer();
+        StringBuffer thingkingResult = new StringBuffer();
+        client.send(messages, new AbstractMassWebSocketListener() {
+            @Override
+            public void onSuccess(WebSocket webSocket, MassResponse resp) {
+                logger.debug("中间返回json结果 ==>{}", JSONUtil.toJsonStr(resp));
+                if (resp.getHeader().getCode() != 0) {
+                    logger.error("code=>{}，error=>{}，sid=>{}", resp.getHeader().getCode(), resp.getHeader().getMessage(), resp.getHeader().getSid());
+                    logger.warn("错误码查询链接：https://www.xfyun.cn/doc/spark/%E7%B2%BE%E8%B0%83%E6%9C%8D%E5%8A%A1API-websocket.html");
+                    return;
+                }
+
+                if (null != resp.getPayload() && null != resp.getPayload().getChoices()) {
+                    List<MassResponse.Payload.Choices.Text> text = resp.getPayload().getChoices().getText();
+                    if (null != text && !text.isEmpty()) {
+                        String content = resp.getPayload().getChoices().getText().get(0).getContent();
+                        String reasonContent = resp.getPayload().getChoices().getText().get(0).getReasoning_content();
+                        if (!StringUtils.isNullOrEmpty(reasonContent)) {
+                            thingkingResult.append(reasonContent);
+                            logger.info("思维链结果... ==> {}", reasonContent);
+                        } else if (!StringUtils.isNullOrEmpty(content)) {
+                            finalResult.append(content);
+                            logger.info("中间结果 ==> {}", content);
+                        }
+                    }
+
+                    if (resp.getPayload().getChoices().getStatus() == 2) {
+                        // 说明数据全部返回完毕，可以关闭连接，释放资源
+                        logger.info("session end");
+                        Date dateEnd = new Date();
+                        logger.info("{}开始", sdf.format(dateBegin));
+                        logger.info("{}结束", sdf.format(dateEnd));
+                        logger.info("耗时：{}ms", dateEnd.getTime() - dateBegin.getTime());
+                        logger.info("完整思维链结果 ==> {}", thingkingResult);
+                        logger.info("最终识别结果 ==> {}", finalResult);
+                        logger.info("本次识别sid ==> {}", resp.getHeader().getSid());
+                        client.closeWebsocket();
+                        System.exit(0);
+                    }
+                }
+            }
+
+            @Override
+            public void onFail(WebSocket webSocket, Throwable t, Response response) {
+                client.closeWebsocket();
+            }
+
+            @Override
+            public void onClose(WebSocket webSocket, int code, String reason) {
+
+            }
+        });
+
+        // post方式
+        // String result = client.send(messages);
+        // logger.debug("{} 模型返回结果 ==>{}", client.getDomain(), result);
+        // JSONObject obj = JSON.parseObject(result);
+        // String content = obj.getJSONArray("choices").getJSONObject(0).getJSONObject("message").getString("content");
+        // logger.info("{} 大模型回复内容 ==>{}", client.getDomain(), content);
+    }
+}
---
+++ src/main/java/cn/xfyun/demo/spark/SparkIatMulLangClientApp.java
@@ -0,0 +1,194 @@
+package cn.xfyun.demo.spark;
+
+import cn.xfyun.api.SparkIatClient;
+import cn.xfyun.config.PropertiesConfig;
+import cn.xfyun.config.SparkIatModelEnum;
+import cn.xfyun.model.sparkiat.response.SparkIatResponse;
+import cn.xfyun.service.sparkiat.AbstractSparkIatWebSocketListener;
+import cn.xfyun.util.StringUtils;
+import okhttp3.Response;
+import okhttp3.WebSocket;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.security.SignatureException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+
+/**
+ * SPARK_MUL_LANG_IAT( iFly Spark Auto Transform ) 多语种大模型语音听写
+ * 1、APPID、APISecret、APIKey信息获取：<a href="https://console.xfyun.cn/services/bmm">...</a>
+ * 2、文档地址：<a href="https://www.xfyun.cn/doc/spark/spark_mul_cn_iat.html">...</a>
+ */
+public class SparkIatMulLangClientApp {
+    private static final Logger logger = LoggerFactory.getLogger(SparkIatMulLangClientApp.class);
+    private static final String appId = PropertiesConfig.getAppId();
+    private static final String apiKey = PropertiesConfig.getApiKey();
+    private static final String apiSecret = PropertiesConfig.getApiSecret();
+
+    private static final String filePath = "audio/spark_iat_mul_lang_16k_10.pcm";
+    private static String resourcePath;
+
+    static {
+        try {
+            resourcePath = Objects.requireNonNull(SparkIatZhClientApp.class.getResource("/")).toURI().getPath();
+        } catch (URISyntaxException e) {
+            logger.error("获取资源路径失败", e);
+        }
+    }
+
+
+    public static void main(String[] args) throws FileNotFoundException, SignatureException, MalformedURLException, InterruptedException {
+        SparkIatClient sparkIatClient = new SparkIatClient.Builder()
+                .signature(appId, apiKey, apiSecret, SparkIatModelEnum.MUL_CN_MANDARIN.getCode())
+                // 流式实时返回撰写结果
+                .dwa("wpgs")
+                .build();
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyy-MM-dd HH:mm:ss.SSS");
+        Date dateBegin = new Date();
+
+        File file = new File(resourcePath + filePath);
+        StringBuffer finalResult = new StringBuffer();
+
+        // 存储流式返回结果的Map sn -> content
+        Map<Integer, String> contentMap = new TreeMap<>();
+        AtomicBoolean isEnd = new AtomicBoolean(false);
+        sparkIatClient.send(file, new AbstractSparkIatWebSocketListener() {
+            @Override
+            public void onSuccess(WebSocket webSocket, SparkIatResponse resp) {
+                // logger.debug("{}", JSON.toJSONString(resp));
+                if (resp.getHeader().getCode() != 0) {
+                    logger.error("code=>{}，error=>{}，sid=>{}", resp.getHeader().getCode(), resp.getHeader().getMessage(), resp.getHeader().getSid());
+                    logger.warn("错误码查询链接：https://www.xfyun.cn/document/error-code");
+                    return;
+                }
+
+                if (resp.getPayload() != null) {
+                    // 非流式实时返回结果处理方式
+                    /*if (resp.getPayload().getResult() != null) {
+                        String tansTxt = resp.getPayload().getResult().getText();
+                        if (null != tansTxt) {
+                            // 解码转写结果
+                            byte[] decodedBytes = Base64.getDecoder().decode(resp.getPayload().getResult().getText());
+                            String decodeRes = new String(decodedBytes, StandardCharsets.UTF_8);
+                            SparkIatResponse.JsonParseText jsonParseText = StringUtils.gson.fromJson(decodeRes, SparkIatResponse.JsonParseText.class);
+
+                            StringBuilder text = new StringBuilder();
+                            for (SparkIatResponse.Ws ws : jsonParseText.getWs()) {
+                                List<SparkIatResponse.Cw> cwList = ws.getCw();
+                                for (SparkIatResponse.Cw cw : cwList) {
+                                    text.append(cw.getW());
+                                }
+                            }
+
+                            finalResult.append(text);
+                            logger.info("中间识别结果 ==>{}", text);
+                        }
+                    }*/
+
+                    // 流式实时返回结果处理方式
+                    if (null != resp.getPayload().getResult().getText()) {
+                        byte[] decodedBytes = Base64.getDecoder().decode(resp.getPayload().getResult().getText());
+                        String decodeRes = new String(decodedBytes, StandardCharsets.UTF_8);
+                        // logger.info("中间识别结果 ==>{}", decodeRes);
+                        SparkIatResponse.JsonParseText jsonParseText = StringUtils.gson.fromJson(decodeRes, SparkIatResponse.JsonParseText.class);
+                        // 拼接单句ws的内容
+                        StringBuilder reqResult = getWsContent(jsonParseText);
+                        // 根据pgs参数判断是拼接还是替换
+                        if (jsonParseText.getPgs().equals("apd")) {
+                            // 直接添加
+                            contentMap.put(jsonParseText.getSn(), reqResult.toString());
+                            logger.info("中间识别结果 【{}】 拼接后结果==> {}", reqResult, getLastResult(contentMap));
+                        } else if (jsonParseText.getPgs().equals("rpl")) {
+                            List<Integer> rg = jsonParseText.getRg();
+                            int startIndex = rg.get(0);
+                            int endIndex = rg.get(1);
+                            // 替换 rg 范围内的内容
+                            for (int i = startIndex; i <= endIndex; i++) {
+                                contentMap.remove(i);
+                            }
+                            contentMap.put(jsonParseText.getSn(), reqResult.toString());
+                            logger.info("中间识别结果 【{}】 替换后结果==> {}", reqResult, getLastResult(contentMap));
+                        }
+                    }
+
+                    if (resp.getPayload().getResult().getStatus() == 2) {
+                        // resp.data.status ==2 说明数据全部返回完毕，可以关闭连接，释放资源
+                        logger.info("session end");
+                        Date dateEnd = new Date();
+                        logger.info("{}开始", sdf.format(dateBegin));
+                        logger.info("{}结束", sdf.format(dateEnd));
+                        logger.info("耗时：{}ms", dateEnd.getTime() - dateBegin.getTime());
+                        if (!contentMap.isEmpty()) {
+                            // 获取最终拼接结果
+                            logger.info("最终识别结果 ==>{}", getLastResult(contentMap));
+                        } else {
+                            logger.info("最终识别结果 ==>{}", finalResult);
+                        }
+                        logger.info("本次识别sid ==>{}", resp.getHeader().getSid());
+                        sparkIatClient.closeWebsocket();
+                        isEnd.set(Boolean.TRUE);
+                        System.exit(0);
+                    }
+                }
+            }
+
+            @Override
+            public void onFail(WebSocket webSocket, Throwable t, Response response) {
+                isEnd.set(Boolean.TRUE);
+                logger.error("异常信息: {}", t.getMessage(), t);
+                System.exit(0);
+            }
+
+            @Override
+            public void onClose(WebSocket webSocket, int code, String reason) {
+                isEnd.set(Boolean.TRUE);
+                logger.info("关闭连接,code是{},reason:{}", code, reason);
+                System.exit(0);
+            }
+        });
+        // 多语种没有流式返回会开启线程动态打印日志
+        Thread loading = new Thread(() -> {
+            while (!isEnd.get()) {
+                try {
+                    TimeUnit.MILLISECONDS.sleep(2500);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                logger.info("音频识别中...");
+            }
+        });
+        loading.setName("Logger-SparkIatMulLangClientApp-Thread");
+        loading.start();
+    }
+
+    private static String getLastResult(Map<Integer, String> contentMap) {
+        StringBuilder result = new StringBuilder();
+        for (String part : contentMap.values()) {
+            result.append(part);
+        }
+        return result.toString();
+    }
+
+    private static StringBuilder getWsContent(SparkIatResponse.JsonParseText jsonParseText) {
+        StringBuilder reqResult = new StringBuilder();
+        List<SparkIatResponse.Ws> wsList = jsonParseText.getWs();
+        for (SparkIatResponse.Ws ws : wsList) {
+            List<SparkIatResponse.Cw> cwList = ws.getCw();
+            for (SparkIatResponse.Cw cw : cwList) {
+                // logger.info(cw.getW());
+                reqResult.append(cw.getW());
+            }
+        }
+        return reqResult;
+    }
+}
