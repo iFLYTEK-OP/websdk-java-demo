7db1abe79932eb312b33e3c184e6f3fcaa9fa13f
zyding6 <zyding6@iflytek.com>
2025-04-14 14:10:27 +0800

    fixed: 去除语音合成的录音播放功能

--- src/main/java/cn/xfyun/demo/speech/TtsClientApp.java
+++ src/main/java/cn/xfyun/demo/speech/TtsClientApp.java
@@ -4,7 +4,6 @@ import cn.xfyun.api.TtsClient;
 import cn.xfyun.config.PropertiesConfig;
 import cn.xfyun.model.response.TtsResponse;
 import cn.xfyun.service.tts.AbstractTtsWebSocketListener;
-import cn.xfyun.util.AudioPlayer;
 import okhttp3.Response;
 import okhttp3.WebSocket;
 
@@ -13,12 +12,11 @@ import java.io.FileNotFoundException;
 import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 import java.security.SignatureException;
-import java.util.Base64;
 
 /**
  * TTS ( Text to Speech ):语音合成
- 1、APPID、APISecret、APIKey信息获取：https://console.xfyun.cn/services/tts
- 2、文档地址：https://www.xfyun.cn/doc/tts/online_tts/API.html
+ * 1、APPID、APISecret、APIKey信息获取：https://console.xfyun.cn/services/tts
+ * 2、文档地址：https://www.xfyun.cn/doc/tts/online_tts/API.html
  */
 public class TtsClientApp {
 
@@ -36,11 +34,6 @@ public class TtsClientApp {
 
         File file = new File(resourcePath + filePath);
         try {
-
-            // 开启语音实时播放
-            AudioPlayer audioPlayer = new AudioPlayer();
-            audioPlayer.start();
-
             ttsClient.send("语音合成流式接口将文字信息转化为声音信息", new AbstractTtsWebSocketListener(file) {
                 @Override
                 public void onSuccess(byte[] bytes) {
@@ -49,34 +42,14 @@ public class TtsClientApp {
                 @Override
                 public void onFail(WebSocket webSocket, Throwable throwable, Response response) {
                     System.out.println(throwable.getMessage());
-                    audioPlayer.stop();
                 }
 
                 @Override
                 public void onBusinessFail(WebSocket webSocket, TtsResponse ttsResponse) {
                     System.out.println(ttsResponse.toString());
-                    audioPlayer.stop();
-                }
-
-                @Override
-                public void onMessage(WebSocket webSocket, String text) {
-                    super.onMessage(webSocket, text);
-                    TtsResponse resp = JSON.fromJson(text, TtsResponse.class);
-                    if (resp != null) {
-                        if (resp.getCode() != 0) {
-                            onBusinessFail(webSocket, resp);
-                        }
-                        if (resp.getData() != null) {
-                            String result = resp.getData().getAudio();
-                            if (result != null) {
-                                byte[] audio = Base64.getDecoder().decode(result);
-                                audioPlayer.play(audio);
-                            }
-                        }
-                    }
                 }
             });
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e.getMessage());
             System.out.println("错误码查询链接：https://www.xfyun.cn/document/error-code");
         }
